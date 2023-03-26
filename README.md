bd5e3c048ba36f138a0f8f1427e35e933f98f048
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-03-26 13:58:51 -0400

    Added giphy api to all functions to display gifs, adjusted format to have clean ui on all files, added comments, and edited readme to include image of page

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-Travel presents a lot of great opportunities to you, including making your friends jealous! Giphy Travel Guide allows you to search different travel destinations and have it present you with Gifs related to your searched destination. 
+Travel presents a lot of great opportunities to you, including making your family and friends jealous! Giphy Travel Guide allows you to search different travel destinations and have it present you with gifs related to your searched destination. You can use this to see the traditions, cultures or popular events for your searched destination. You can also use the site to get prepared with the gifs you will send to your family and friends!
 
 Working on this project, we were able to learn how to code as a team. We were able to learn about different APIs including the Google Maps API, the Giphy API, TailWind CSS, and Jquery. By building our site from scratch, we were able to experience every aspect of development. We were able to come up with the idea of the site, the tasks that needed to be done, the division of responsibilities, and ensuring all tasks are completed. 
 
@@ -12,13 +12,16 @@ N/A
 
 ## Usage
 
-You can use the searchbar on the map to search for a destination. If you are undecided, you can use the Surprise Me button to randomly select a top travel destination based on Lonely Planet's Best Places to Visit in 2023. 
+- You can use the searchbar on the map to search for a destination. 
+- If you are undecided, you can use the Surprise Me button to randomly select a top travel destination based on [Lonely Planet's Best Places to Visit in 2023](https://www.lonelyplanet.com/best-in-travel). 
+- When you search a destination, you can revisit the previous searches by clicking on the flag pins on the map with the previous destinations. 
+- You can use the Search Again button to search a new destination or view previous searches. 
+- In the Footer, you will find links to the [Lonely Planet's Best Places to Visit in 2023](https://www.lonelyplanet.com/best-in-travel) and a [Travel Shop](https://mytravelshop.ca/) to better assist you with your travel planning needs.
+- Also in the Footer, you will find links to the creators of the site's GitHubs.
 
-When you search a destination, you can revisit the previous searches by clicking on the flag pins on the map with the previous destinations. 
+You can visit the deployed [Giphy Travel Guide Page here.](https://mdeluca13.github.io/giphy-travel-guide/)
 
-You can use the Search Again button to search a new destination. 
-
-You can visit the deployed [Giphy Travel Guide Page here](https://mdeluca13.github.io/giphy-travel-guide/)
+![Giphy Travel Guide Home Page](/assets/images/Giphy-Travel-Guide-Home.png)
 
 ## Credits
 
@@ -39,6 +42,7 @@ APIs Used:
 Resources Used:
 
 - [Best Places to Visit in 2023 - Lonely Planet](https://www.lonelyplanet.com/best-in-travel)
+- [Travel Shop](https://mytravelshop.ca/)
 
 ## License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
-// maybe flag local restaurants or tourist spots
-// there is also a function called getCenter() that gives lat/long of the center of the map
-// could use for searching through the map
-// maybe find a way to click on map to drop pin
-
 // @license
 // Copyright 2019 Google LLC. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Declaring Variables 
 var surpriseBtn = $('#surprise-btn');
 var searchAgainBtn = $('#search-again-btn')
 var clearStorage = $('#clear-storage-btn')
@@ -29,235 +25,211 @@ var autocomplete;
 var marker;
 var storedInfo = [];
 var surpriseList = [
-    ['Lima, Peru', -12.0463731, -77.042754],
-    ['Umbria, Italy', 42.938004, 12.6216211],
-    ['Fukuoka, Japan', 33.5901838, 130.4016888],
-    ['Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia', 3.1569486, 101.712303],
-    ['South Africa', -30.559482, 22.937506],
-    ['Nova Scotia, Canada', 44.6922613, -62.6571885],
-    ['Bhutan', 27.514162, 90.433601],
-    ['Magdalena, Columbia', 11.3064409, -74.0657561],
-    ['Istanbul, Turkey', 41.0082376, 28.9783589],
-    ['Western Australia, Australia', -27.6728168, 121.6283098],
-    ['Zambia', -13.133897, 27.849332],
-    ['Dominica', 15.414999,-61.37097600000001],
-    ['Halkidiki, Greece', 40.3694997,23.287085],
-    ['Jamaica', 18.109581, -77.297508],
-    ['Jordan', 30.585164, 36.238414],
-    ['Malta', 35.937496, 14.375416],
-    ['Raja Ampat Regency, West Papua, Indonesia', -1.032046750338687, 130.5052175718387],
-    ['Accra, Ghana', 5.6037168, -0.1869644],
-    ['Albania', 41.153332, 20.168331],
-    ['Sydney NSW, Australia', -33.8688197, 151.2092955],
-    ['Guyana', 4.860416, -58.93018],
-    ['Boise, ID, USA', 43.6150186, -116.2023137],
-    ['Alaska, USA', 64.2008413, -149.4936733],
-    ['New Mexico, USA', 34.5199402, -105.8700901],
-    ['El Salvador', 13.794185, -88.89653],
-    ['Dresden, Germany', 51.0504088, 13.7372621],
-    ['Marseille, France', 43.296482, 5.36978],
-    ['Manchester, UK', 53.4807593, -2.2426305],
-    ['Scotland, UK', 56.49067119999999, -4.2026458],
-    ['Montevideo, Uruguay', -34.8181587, -56.2138256]
+  ['Lima, Peru', -12.0463731, -77.042754, 'Peru'],
+  ['Umbria, Italy', 42.938004, 12.6216211, 'Italy'],
+  ['Fukuoka, Japan', 33.5901838, 130.4016888, 'Japan'],
+  ['Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia', 3.1569486, 101.712303, 'Malaysia'],
+  ['South Africa', -30.559482, 22.937506, 'South Africa'],
+  ['Nova Scotia, Canada', 44.6922613, -62.6571885, 'Canada'],
+  ['Bhutan', 27.514162, 90.433601, 'Bhutan'],
+  ['Magdalena, Columbia', 11.3064409, -74.0657561, 'Columbia Landscape'],
+  ['Istanbul, Turkey', 41.0082376, 28.9783589, 'Istanbul'],
+  ['Western Australia, Australia', -27.6728168, 121.6283098, 'Western Australia'],
+  ['Zambia', -13.133897, 27.849332, 'Zambia'],
+  ['Dominica', 15.414999,-61.37097600000001, 'Dominican Nature'],
+  ['Halkidiki, Greece', 40.3694997,23.287085, 'Greece'],
+  ['Jamaica', 18.109581, -77.297508, 'Jamaica'],
+  ['Jordan', 30.585164, 36.238414, 'Jordan Tourism'],
+  ['Malta', 35.937496, 14.375416, 'Malta Island Nature'],
+  ['Raja Ampat Regency, West Papua, Indonesia', -1.032046750338687, 130.5052175718387, 'Indonesia'],
+  ['Accra, Ghana', 5.6037168, -0.1869644, 'Ghana'],
+  ['Albania', 41.153332, 20.168331, 'Albania Tourism'],
+  ['Sydney NSW, Australia', -33.8688197, 151.2092955, 'Sydney Tourism'],
+  ['Guyana', 4.860416, -58.93018, 'Guyana'],
+  ['Boise, ID, USA', 43.6150186, -116.2023137, 'Idaho'],
+  ['Alaska, USA', 64.2008413, -149.4936733, 'Alaska Nature'],
+  ['New Mexico, USA', 34.5199402, -105.8700901, 'New Mexico'],
+  ['El Salvador', 13.794185, -88.89653, 'El Salvador'],
+  ['Dresden, Germany', 51.0504088, 13.7372621, 'Germany'],
+  ['Marseille, France', 43.296482, 5.36978, 'France'],
+  ['Manchester, UK', 53.4807593, -2.2426305, 'Manchester Travel'],
+  ['Scotland, UK', 56.49067119999999, -4.2026458, 'Scotland'],
+  ['Montevideo, Uruguay', -34.8181587, -56.2138256, 'Uruguay']
 ];
 
 // Getting local storage through key length and getting key name and latlng
 for (var i = 0; i < localStorage.length; i++) {
 
-    var key = localStorage.key(i);
-    var value = localStorage.getItem(key);
-    value = value.replaceAll('"','');
-    value = value.split(',');
+  var key = localStorage.key(i);
+  var value = localStorage.getItem(key);
+  value = value.replaceAll('"','');
+  value = value.split(',');
 
-    storedLat = Number(value[0]);
-    storedLng = Number(value[1]);
-    // console.log('city: ' + key + '; lat: ' + storedLat + '; lng: ' + storedLng)
-    storedInfo[i] = [key, storedLat, storedLng, i];
+  storedLat = Number(value[0]);
+  storedLng = Number(value[1]);
+  storedInfo[i] = [key, storedLat, storedLng, i];
 
-}
+};
 
 // Sets the previous searched places to be listed in the map as flags
 function setMarkers(map) {
 
-    var image = {
-      url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-      size: new google.maps.Size(20, 32),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(0, 32),
-    };
+  var image = {
+    url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    size: new google.maps.Size(20, 32),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(0, 32),
+  };
 
-    var shape = {
-      coords: [1, 1, 1, 20, 18, 20, 18, 1],
-      type: "poly",
-    };
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: "poly",
+  };
 
-    for (var i = 0; i < storedInfo.length; i++) {
+  for (var i = 0; i < storedInfo.length; i++) {
 
-      let marker = new google.maps.Marker({
-        position: { lat: storedInfo[i][1], lng: storedInfo[i][2] },
-        map,
-        icon: image,
-        shape: shape,
-        latitude: storedInfo[i][1],
-        longitude: storedInfo[i][2],
-        title: storedInfo[i][0],
-        zIndex: storedInfo[i][3],
+    let marker = new google.maps.Marker({
+      position: { lat: storedInfo[i][1], lng: storedInfo[i][2] },
+      map,
+      icon: image,
+      shape: shape,
+      latitude: storedInfo[i][1],
+      longitude: storedInfo[i][2],
+      title: storedInfo[i][0],
+      zIndex: storedInfo[i][3],
 
+    });
+
+    // Click event listener to the flags to zoom in when clicked on and add red marker
+    marker.addListener('click', function(){
+
+      var clicked = { lat: marker.get("latitude"), lng: marker.get("longitude") };
+      var name = marker.get("title");
+      map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: clicked,
       });
 
-      // Click event listener to the flags to zoom in when clicked on and add red marker
-// need to work on after click still have search bar showing
-      marker.addListener('click', function(){
-
-        var clicked = { lat: marker.get("latitude"), lng: marker.get("longitude") };
-        map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 6,
-          center: clicked,
-        });
-        // card = document.getElementById("pac-card");
-        // input = document.getElementById("pac-input");
-        // map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
-
-        // autocomplete = new google.maps.places.Autocomplete(input);
-
-        marker = new google.maps.Marker({
-          position: clicked,
-          map: map,
-        });
-
-
-  //       autocomplete.addListener("place_changed", () => {
-  //         marker.setVisible(false);
-
-  //         place = autocomplete.getPlace();
-
-  //         placeID = place.place_id;
-  //         placeName = place.formatted_address;
-
-  //         if (!place.geometry || !place.geometry.location) {
-  //         // User entered the name of a Place that was not suggested and
-  //         // pressed the Enter key, or the Place Details request failed.
-
-  // // CHANGE THIS TO MODAL AND ADD GIF
-  //             window.alert("No details available for input: '" + place.name + "'");
-  //             return;
-  //         }
-
-  //       // If the place has a geometry, then present it on a map.
-  //         if (place.geometry.viewport) {
-  //             map.fitBounds(place.geometry.viewport);
-  //         } else {
-  //             map.setCenter(place.geometry.location);
-  //             map.setZoom(17);
-  //         }
-
-  //         marker.setPosition(place.geometry.location);
-  //         marker.setVisible(true);
-
-  //     });
-      })
-    }
-}
+      console.log(name)
+      gifLoad('travel ' + name);
+      marker = new google.maps.Marker({
+        position: clicked,
+        map: map,
+      });
+    });
+  };
+};
 
 // map function to display map
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 0, lng: 0 },
-      zoom: 1,
-      mapTypeControl: false,
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 0, lng: 0 },
+    zoom: 1,
+    mapTypeControl: false,
+  });
 
-    if (localStorage.length !== 0){
-        setMarkers(map);
+  if (localStorage.length !== 0){
+    setMarkers(map);
+  };
+
+  card = document.getElementById("pac-card");
+  input = document.getElementById("pac-input");
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+
+  autocomplete = new google.maps.places.Autocomplete(input);
+
+  marker = new google.maps.Marker({
+    map,
+    anchorPoint: new google.maps.Point(0, 0),
+  });
+
+  autocomplete.addListener("place_changed", () => {
+    marker.setVisible(false);
+
+    place = autocomplete.getPlace();
+        
+    placeID = place.place_id;
+    placeName = place.formatted_address;
+
+    if (!place.geometry || !place.geometry.location) {
+
+      // User entered the name of a Place that was not suggested and pressed the Enter key, or the Place Details request failed
+      // Modal to display error message 
+      var modal = document.getElementById("myModal");
+      var span = document.getElementsByClassName("close")[0];
+      var modalContent = $('#modal-gif');
+
+      modal.style.display = "block";
+      document.querySelector("#modal_text").innerHTML = "No details available for input: '" + place.name + "'";
+          
+      fetch ('https://api.giphy.com/v1/gifs/search?api_key=AXlVnQ0kNMbPNVXrSOZm0MPgYw1z1egm&q=error&limit=20')
+      .then (function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var i = 0; i < 3; i++){
+          var imgPath = (data.data[i].images.original.url);
+          var img = $('<img>');
+          img.attr('src', imgPath);
+          img.attr('alt', 'Error Gif')
+          img.css('width', '150px');
+          img.css('height', '150px');
+          img.css('border-radius', '30px');
+          img.css('padding', '10px');
+          modalContent.append(img);
+        };
+      });
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+      modal.style.display = "none";
+      };
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        };
+      };
+      return;
     };
 
-    card = document.getElementById("pac-card");
-    input = document.getElementById("pac-input");
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } 
+    else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    };
 
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
-
-    autocomplete = new google.maps.places.Autocomplete(input);
-
-    marker = new google.maps.Marker({
-      map,
-      anchorPoint: new google.maps.Point(0, 0),
-    });
-
-    autocomplete.addListener("place_changed", () => {
-        marker.setVisible(false);
-
-        place = autocomplete.getPlace();
-
-        gifSearch = place.address_components[0].long_name
-
-        gifLoad(gifSearch)
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+    gifSearch = place.address_components[0].long_name;
+    gifLoad(gifSearch + ' Tourism');
         
-        placeID = place.place_id;
-        placeName = place.formatted_address;
-
-        // getting lat lng
-        fetch ('https://maps.googleapis.com/maps/api/geocode/json?place_id=' + placeID + '&key=AIzaSyBtgd8tFi13h7xX6R0bVuKhWddciq5pd94')
-        .then (function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            lat = data.results[0].geometry.location.lat;
-            lng = data.results[0].geometry.location.lng;
-            latLng = lat + ',' + lng;
-            saveSearch();
-        });
-
-        if (!place.geometry || !place.geometry.location) {
-        // User entered the name of a Place that was not suggested and
-        // pressed the Enter key, or the Place Details request failed.
-
-        // Code for Modal
-
-        var modal = document.getElementById("myModal");
-
-        var span = document.getElementsByClassName("close")[0];
-
-        modal.style.display = "block";
-
-        document.querySelector("#modal_text").innerHTML = "No details available for input: '" + place.name + "'";
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
-
-        return;
-        }
-
-      // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
-
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-
+    // getting lat lng
+    fetch ('https://maps.googleapis.com/maps/api/geocode/json?place_id=' + placeID + '&key=AIzaSyBtgd8tFi13h7xX6R0bVuKhWddciq5pd94')
+    .then (function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      lat = data.results[0].geometry.location.lat;
+      lng = data.results[0].geometry.location.lng;
+      latLng = lat + ',' + lng;
+      saveSearch();
     });
-
-}
+  });
+};
 
 // Calling Map function to display
 window.initMap = initMap;
 
 // Save last search to local storage function
 function saveSearch() {
-    localStorage.setItem(placeName, JSON.stringify(latLng));
-}
+  localStorage.setItem(placeName, JSON.stringify(latLng));
+};
 
 // Clear storage click listener
 clearStorage.on('click', function () {
@@ -268,14 +240,16 @@ clearStorage.on('click', function () {
 // randomizer for click event surprise me
 surpriseBtn.on('click', function () {
   var random = Math.floor(Math.random() * 30); //<< gets random number from 0 - 29 for index
-  console.log(surpriseList[random][1])
-
   var rdmPlace = { lat: surpriseList[random][1], lng: surpriseList[random][2] };
+  var latLngRandom = surpriseList[random][1] + ',' + surpriseList[random][2];
+  localStorage.setItem(surpriseList[random][0], JSON.stringify(latLngRandom));
 
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
     center: rdmPlace,
   });
+
+  gifLoad(surpriseList[random][3]);
 
   marker = new google.maps.Marker({
     position: rdmPlace,
@@ -288,19 +262,27 @@ searchAgainBtn.on('click', function () {
   location.reload();
 });
 
+// Gif function to display gif of input at bottom of the page
 function gifLoad(input) {
-  
+  container.empty();
   fetch ('https://api.giphy.com/v1/gifs/search?api_key=AXlVnQ0kNMbPNVXrSOZm0MPgYw1z1egm&q=' + input + '&limit=20')
   .then (function (response) {
     return response.json();
   })
   .then(function (data) {
-    var imgPath = (data.data[0].images.original.url)
-    var img = $('<img>')
-    img.attr('src', imgPath)
-    img.css('width', '100px')
-    img.css('height', '100px')
-
-  container.append(img)
+    for (var i = 0; i < 6; i++){
+      var imgPath = (data.data[i].images.original.url);
+      var img = $('<img>');
+      img.attr('src', imgPath);
+      img.attr('alt', input + ' Gif')
+      img.css('width', '250px');
+      img.css('height', '250px');
+      img.css('border-radius', '30px');
+      img.css('padding', '10px');
+      container.append(img);
+    };
   });
-}
+};
+
+// Calling gifLoad on homepage with input of travel
+gifLoad('travel');
